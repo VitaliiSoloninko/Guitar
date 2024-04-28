f1994474fc0dd8aae394ee0233d50aa70f3b8e63
22051988Di <vitalii.soloninko@gmail.com>
2024-04-28 14:17:46 +0200

    v 1.1	navigation by id

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-"use script"
+'use script'
 
-const reviewsSwiper = document.querySelector('.swiper-reviews');
+// Slider Swiper
+
+const reviewsSwiper = document.querySelector('.swiper-reviews')
 
 if (reviewsSwiper) {
 	const swiper = new Swiper('.swiper-reviews', {
@@ -12,7 +14,19 @@ if (reviewsSwiper) {
 			el: '.swiper-pagination',
 			clickable: true,
 		},
-	});
+	})
 }
 
+// Navigation by ID - anchors in HTML in Header a href="#home" in Section id="home"
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (event) {
+		event.preventDefault() // відмінили стандарте повеение кнопки, зараз вона не працює
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+}
